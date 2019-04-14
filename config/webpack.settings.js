18dbacce323b35740c2d8bc639393657aae434b2
Marten <martenzander@gmail.com>
2019-04-14 20:07:20 +0200

    update webpack settings

--- config/webpack.settings.js
+++ config/webpack.settings.js
@@ -4,13 +4,4 @@ module.exports = {
 	resolve: {
 		extensions: ["*", ".js", ".jsx"],
 	},
-	plugins: {
-		htmlWebpackPlugin: {
-			title: `${packageConfig.name.charAt(0).toUpperCase() +
-				packageConfig.name.slice(1)} – JavaScript Animation Framework`,
-			keys: packageConfig.keywords,
-			description: packageConfig.description,
-			template: "app/src/html/index.html",
-		},
-	},
 };
